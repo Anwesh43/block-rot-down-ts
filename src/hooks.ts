19b83616ee8b58bdd4ec446fee638dd06c56a529
Anwesh43 <anweshthecool0@gmail.com>
2022-11-14 21:12:31 +0530

    adding useStyle hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react'
+import {useState, useEffect, CSSProperties} from 'react'
 
 export const useAnimatedScale = (scGap : number = 0.01 , delay : number = 20) => {
     const [scale, setScale] = useState(0)
@@ -32,12 +32,45 @@ export const useDimension = () => {
         }
         return () => {
             window.onresize = () => {
-                
+
             }
         }
     }, [])
     return {
         w, 
         h 
+    }
+}
+
+const sinify : (a : number) => number = (scale : number) : number => Math.sin(scale * Math.PI)
+const maxScale : (sc : number, i : number, n : number) => number = (scale : number, i : number, n : number) : number => Math.max(0, scale - i / n)
+const divideScale : (sc : number, i : number, n : number) => number = (scale : number, i : number, n : number) : number => Math.min(1 / n, maxScale(scale, i, n)) * n 
+
+
+export const useStyle = (w : number, h : number, scale : number) => {
+    const position = 'absolute'
+    const background = 'indigo'
+    const size = Math.min(w, h) / 10 
+    const x = w / 2
+    const y = h / 2
+    const sf : number = sinify(scale)
+    return {
+        parentStyle() : CSSProperties {
+            return {
+                position, 
+                left: `${x}px`,
+                top: `${y + (h / 2) * divideScale(sf, 1, 2)}px`,
+                transform: `rotate(${90 * divideScale(sf, 0, 2)}deg)`
+            }
+        },
+
+        blockStyle() : CSSProperties {
+            return {
+                position,
+                width: `${size}px`,
+                height: `${size}px`,
+                background
+            }
+        }
     }
 }
